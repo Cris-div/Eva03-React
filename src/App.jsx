f76ef5d146e5a8151156d037b0137c83d5e5fb95
Cris-div <christian.unocc@tecsup.edu.pe>
2025-11-05 21:30:36 -0500

    Implementación de React Router para navegación

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home.jsx'
+import Entities from './pages/Entities.jsx'
 
 function App() {
   return (
     <>
-      Rick and Morty API
+    <Router>
+      <nav className="bg-gray-800 text-white p-4 flex justify-center gap-4">
+        <Link to="/">Home</Link>
+        <Link to="/entities">Entidades</Link> 
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/entities" element={<Entities />} />
+      </Routes>
+    </Router>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,20 +15,19 @@ function Home (){
     return (
         <>
         <div className="p-6 text-center">
-      <h1 className="text-4xl font-bold mb-4">🛸 Rick & Morty Universe</h1>
-      <p className="text-gray-600 mb-6">
-        Explora personajes, lugares y episodios del universo de Rick & Morty.
-      </p>
-
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        {characters.map((char) => (
-          <div key={char.id} className="p-4 border rounded-xl shadow">
-            <img src={char.image} alt={char.name} className="rounded-lg" />
-            <h2 className="mt-2 font-semibold">{char.name}</h2>
-          </div>
-        ))}
-      </div>
-    </div>
+            <h1 className="text-4xl font-bold mb-4">🛸 Rick & Morty Universe</h1>
+            <p className="text-gray-600 mb-6">
+                Explora personajes, lugares y episodios del universo de Rick & Morty.
+                </p>
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                {characters.map((char) => (
+                    <div key={char.id} className="p-4 border rounded-xl shadow">
+                    <img src={char.image} alt={char.name} className="rounded-lg" />
+                    <h2 className="mt-2 font-semibold">{char.name}</h2>
+                    </div>
+                ))}
+            </div>
+        </div>
         </>
     );
 }
